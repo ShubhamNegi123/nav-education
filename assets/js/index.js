cbae42da07e46b93420c254865bb3606cffed5ed
Shubham Negi <95840309+ShubhamNegi123@users.noreply.github.com>
2022-02-18 11:16:36 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,154 +1,84 @@
-$(function () {
+$(function(){
+  
 
-    homeAnimation();
-
-    //student owl carosals
-    $('.testimonial-owl-carosal,.blog-owl-carosal').owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplaySpeed: 2000,
-        slideTransition: 'linear',
-        autoplayHoverPause: true,
-        // navText : [
-        //     'prev',
-        //     'next'
-        // ],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
-        }
-    })
+let productOfTheDayImg = $('.today-product-img .img1').attr('src');
+$('.today-product-details .images img').mouseenter(function(){
+    let hoverImg = $(this).attr('src');
+    $('.today-product-img .img1').attr('src',hoverImg);
+});
 
 
-    $('.countries-we-served-slider').owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        autoplaySpeed: 3000,
-        slideTransition: 'linear',
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 2
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 4
-            },
-            1500: {
-                items: 5
-            }
-        }
-    })
 
 
-    $('.avilable-in-countries-slider').owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        autoplaySpeed: 3000,
-        slideTransition: 'linear',
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    })
 
-    // counter up initilization
     $('.number').counterUp({
         delay: 10,
         time: 1000
     });
 
-    // aos library initilization
-    AOS.init();
+    $('.testimonial').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:false,
+        dots: true,
+        autoplay:false,
+        autoplayTimeout:500,
+        autoplayHoverPause:false,
+        smartSpeed: 500,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:2
+            },
+            1300:{
+                items:3
+            }
+        }
+    });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:false,
+        smartSpeed: 2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    
 
-    // all home animation
-    function homeAnimation() {
-        // tower animation
-        $('.home .home-banner-heading .tower').animate({
-            bottom: '0px',
-            height: '492px'
-        }, 2500);
-
-        // tild-building animation
-        $('.home .home-banner-heading .tild-building').animate({
-            bottom: '0px',
-            height: '252px'
-        }, 2500);
-
-
-        // statue animation
-        $('.home .home-banner-heading .statue').animate({
-            bottom: '0px',
-            height: '364px'
-        }, 2500);
-
-
-        // wonders animation
-        $('.home .home-banner-heading .wonders').animate({
-            bottom: '0px',
-            height: '258px'
-        }, 2500);
-
-
-        // ballon-left animation
-        $('.home .home-banner-heading .ballon-left').animate({
-            top: '175px'
-        }, 2500);
-
-
-        // ballon-bottom animation
-        $('.home .home-banner-heading .ballon-bottom').animate({
-            top: '480px'
-        }, 2500);
-
-
-        // ellipse animation
-        $('.home .home-banner-heading .ellipse').animate({
-            top: '140px'
-        }, 2500);
-
-
-        // ballon-right animation
-        $('.home .home-banner-heading .ballon-right').animate({
-            top: '30%'
-        }, 2500);
-
-        // left-plan-img animation
-        $('.home .home-banner-heading .left-plan-img').animate({
-            top: '240px',
-            left: '0%'
-        }, 2500);
-
-        // right-plan-img animation
-        $('.home .home-banner-heading .right-plan-img').animate({
-            top: '27%',
-            left: '69%'
-        }, 2500);
-    }
+    /* catogery dropdown */
+    $('header .catogery').mouseenter(function(){
+        $('.shop-by-catogery').slideDown();
+    });     
+    /* getting the position and sliding up dropdown */     
+    var CurrentMouseXPostion;
+    var CurrentMouseYPostion;    
+    $(document).mousemove(function(event) {
+        CurrentMouseXPostion = event.pageX;
+        CurrentMouseYPostion = event.pageY;
+        if(CurrentMouseYPostion < 100 || CurrentMouseYPostion > 900){
+            $('.shop-by-catogery').slideUp();
+        }
+        //console.log(CurrentMouseYPostion)
+    });
+   
+    
+   
 });
