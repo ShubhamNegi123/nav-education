61b580a32b547f8e0c5043ace038cc610141a2f7
Shubham Negi <95840309+ShubhamNegi123@users.noreply.github.com>
2022-02-25 17:36:51 +0530

    Add files via upload

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -5,7 +5,14 @@ $(function () {
 
     /* form first name validation */
     $("#first-name").on("blur", function () {
-        if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
+        if ($(this).val() == '') {
+            $('.fname-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.fname-error').text('Please fill out this field');
+
+        } else if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
             $('.fname-error').css({
                 visibility: 'hidden',
                 opacity: 0
@@ -14,7 +21,6 @@ $(function () {
             $('.fname-error').css({
                 visibility: 'visible',
                 opacity: 1,
-                color: 'red'
             });
             $('.fname-error').text('Enter a valid name');
         }
@@ -23,7 +29,14 @@ $(function () {
 
     /* form last name validation */
     $("#last-name").on("blur", function () {
-        if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
+        if ($(this).val() == '') {
+            $('.lname-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.lname-error').text('Please fill out this field');
+
+        } else if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
             $('.lname-error').css({
                 visibility: 'hidden',
                 opacity: 0
@@ -32,7 +45,6 @@ $(function () {
             $('.lname-error').css({
                 visibility: 'visible',
                 opacity: 1,
-                color: 'red'
             });
             $('.lname-error').text('Enter a valid name');
         }
@@ -41,7 +53,14 @@ $(function () {
 
     /* form father name validation */
     $("#father-name").on("blur", function () {
-        if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
+        if ($(this).val() == '') {
+            $('.father-name-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.father-name-error').text('Please fill out this field');
+
+        } else if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
             $('.father-name-error').css({
                 visibility: 'hidden',
                 opacity: 0
@@ -50,7 +69,6 @@ $(function () {
             $('.father-name-error').css({
                 visibility: 'visible',
                 opacity: 1,
-                color: 'red'
             });
             $('.father-name-error').text('Enter a valid name');
         }
@@ -59,7 +77,14 @@ $(function () {
 
     /* form mother name validation */
     $("#mother-name").on("blur", function () {
-        if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
+        if ($(this).val() == '') {
+            $('.mother-name-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.mother-name-error').text('Please fill out this field');
+
+        } else if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
             $('.mother-name-error').css({
                 visibility: 'hidden',
                 opacity: 0
@@ -68,7 +93,6 @@ $(function () {
             $('.mother-name-error').css({
                 visibility: 'visible',
                 opacity: 1,
-                color: 'red'
             });
             $('.mother-name-error').text('Enter a valid name');
         }
@@ -77,7 +101,14 @@ $(function () {
 
     /* form nationality validation */
     $("#nationality").on("blur", function () {
-        if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
+        if ($(this).val() == '') {
+            $('.nationality-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.nationality-error').text('Please fill out this field');
+
+        } else if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
             $('.nationality-error').css({
                 visibility: 'hidden',
                 opacity: 0
@@ -86,7 +117,6 @@ $(function () {
             $('.nationality-error').css({
                 visibility: 'visible',
                 opacity: 1,
-                color: 'red'
             });
             $('.nationality-error').text('Enter a valid country name');
         }
@@ -94,13 +124,17 @@ $(function () {
 
     /* dob validation */
     $('#dob').on("blur", function () {
-        let dob = $('#dob').val();
-        console.log(dob);
-        if (dob == 0) {
+        if ($(this).val() == '') {
             $('.dob-error').css({
                 visibility: 'visible',
                 opacity: 1,
-                color: 'red'
+            });
+            $('.dob-error').text('Please fill out this field');
+
+        } else if ($(this).val() == 0) {
+            $('.dob-error').css({
+                visibility: 'visible',
+                opacity: 1,
             });
             $('.dob-error').text('Enter a valid date of birth');
 
@@ -120,18 +154,354 @@ $(function () {
            \ /
 */
 
+    /* tenth-percentage  */
+    $('#ssc-tenth-percentage').on("blur", function () {
+        if ($(this).val() == '') {
+            $('.ssc-tenth-percentage-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.ssc-tenth-percentage-error').text('Please fill out this field');
 
-    /* ssc-tenth-year */
-    /* $("#ssc-tenth-year").on("blur", function () {
-        let percentage = $("#ssc-tenth-percentage").val();
-        var phoneno = /[1-9]{/;
-        if (percentage.match(phoneno)){
-            alert("success");
-        }else {
-            alert("failer");
+        } else if ($(this).val().match(/^(100(\.0{1,2})?|([0-9]?[0-9](\.[0-9]{1,2})))$/)) {
+            $('.ssc-tenth-percentage-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        } else {
+            $('.ssc-tenth-percentage-error').text('Use the format 00.00');
+            $('.ssc-tenth-percentage-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
         }
-    }); */
+    });
+    /* hsc tenth-percentage */
+    $('#hsc-tenth-percentage').on("blur", function () {
+        if ($(this).val() == '') {
+            $('.hsc-tenth-percentage-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.hsc-tenth-percentage-error').text('Please fill out this field');
 
+        } else if ($(this).val().match(/^(100(\.0{1,2})?|([0-9]?[0-9](\.[0-9]{1,2})))$/)) {
+            $('.hsc-tenth-percentage-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        } else {
+            $('.hsc-tenth-percentage-error').text('Use the format 00.00');
+            $('.hsc-tenth-percentage-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        }
+    });
+    $('#pcb-percentage').on("blur", function () {
+        if ($(this).val() == '') {
+            $('.pcb-percentage-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.pcb-percentage-error').text('Please fill out this field');
+
+        } else if (percentage.match(/^(100(\.0{1,2})?|([0-9]?[0-9](\.[0-9]{1,2})))$/)) {
+            $('.pcb-percentage-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        } else {
+            $('.pcb-percentage-error').text('Use the format 00.00');
+            $('.pcb-percentage-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        }
+    });
+    $('#aggregate').on("blur", function () {
+        if ($(this).val() == '') {
+            $('.aggregate-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.aggregate-error').text('Please fill out this field');
+
+        } else if (percentage.match(/^(100(\.0{1,2})?|([0-9]?[0-9](\.[0-9]{1,2})))$/)) {
+            $('.aggregate-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        } else {
+            $('.aggregate-error').text('Use the format 00.00');
+            $('.aggregate-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        }
+    });
+
+
+
+    /* tenth year */
+    $('#ssc-tenth-year').on("blur", function () {
+        if ($(this).val() == '') {
+            $('.ssc-tenth-year-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.ssc-tenth-year-error').text('Please fill out this field');
+
+        } else if (year.match(/^[0-9]{4}$/)) {
+            $('.ssc-tenth-year-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        } else {
+            $('.ssc-tenth-year-error').text('Enter valid year');
+            $('.ssc-tenth-year-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        }
+    });
+    /* hsc */
+    $('#hsc-tenth-year').on("blur", function () {
+        if ($(this).val() == '') {
+            $('.hsc-tenth-year-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.hsc-tenth-year-error').text('Please fill out this field');
+
+        } else if (year.match(/^[0-9]{4}$/)) {
+            $('.hsc-tenth-year-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        } else {
+            $('.hsc-tenth-year-error').text('Enter valid year');
+            $('.hsc-tenth-year-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        }
+    });
+    $('#neet-exam-year').on("blur", function () {
+        if ($(this).val() == '') {
+            $('.neet-exam-year-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.neet-exam-year-error').text('Please fill out this field');
+
+        } else if (year.match(/^[12][0-9]{3}$/)) {
+            $('.neet-exam-year-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        } else {
+            $('.neet-exam-year-error').text('Enter valid year');
+            $('.neet-exam-year-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        }
+    });
+
+    /* tenth marks */
+    $('#ssc-tenth-marks').on("blur", function () {        
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.neet-exam-year-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.neet-exam-year-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.ssc-tenth-marks-error').text('Enter valid marks');
+            $('.ssc-tenth-marks-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.ssc-tenth-marks-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
+    /* hsc */
+    $('#hsc-tenth-marks').on("blur", function () {        
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.neet-exam-year-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.neet-exam-year-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.hsc-tenth-marks-error').text('Enter valid marks');
+            $('.hsc-tenth-marks-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.hsc-tenth-marks-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
+    $('#neet-marks').on("blur", function () {        
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.neet-marks-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.neet-marks-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.neet-marks-error').text('Enter valid marks');
+            $('.neet-marks-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.neet-marks-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
+
+
+    $('#ssc-marks-out-of').on("blur", function () {        
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.ssc-marks-uot-of-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.ssc-marks-uot-of-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.ssc-marks-uot-of-error').text('Enter valid marks');
+            $('.ssc-marks-uot-of-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.ssc-marks-uot-of-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
+    $('#hsc-marks-out-of').on("blur", function () {        
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.hsc-marks-out-of-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.hsc-marks-out-of-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.hsc-marks-out-of-error').text('Enter valid marks');
+            $('.hsc-marks-out-of-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.hsc-marks-out-of-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
+
+
+    $('#physics').on("blur", function () {
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.physics-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.physics-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.physics-error').text('Enter valid marks');
+            $('.physics-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.physics-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
+    
+
+    $('#chemistry').on("blur", function () {        
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.chemistry-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.chemistry-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.chemistry-error').text('Enter valid marks');
+            $('.chemistry-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.chemistry-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
+
+    $('#biology').on("blur", function () {
+        let stm = $(this).val();
+        stm = parseInt(stm);
+        if ($(this).val() == '') {
+            $('.biology-error').css({
+                visibility: 'visible',
+                opacity: 1,
+            });
+            $('.biology-error').text('Please fill out this field');
+
+        } else if (isNaN(stm)) {
+            $('.biology-error').text('Enter valid marks');
+            $('.biology-error').css({
+                visibility: 'visible',
+                opacity: 1
+            });
+        } else {
+            $('.biology-error').css({
+                visibility: 'hidden',
+                opacity: 0
+            });
+        }
+    });
 
 
 
